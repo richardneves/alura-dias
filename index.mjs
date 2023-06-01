deb8ce2eaec8e0d6ce2accf1bc9a22f48a446387
Richard Neves <richard.albuquerqueneves@gmail.com>
2023-06-01 00:51:57 -0300

    alterando log

--- index.mjs
+++ index.mjs
@@ -2,10 +2,10 @@ import { log } from './log.mjs';
 
 export const handler = async(event) => {
 
-    log('Log de execução. event :' + JSON.stringify(event));
+    log('Log de execução. apos GitHub Action event :' + JSON.stringify(event));
 
     return {
-        statusCode: 200
+        statusCode: 200,
         body: JSON.stringify(event),
     };
 };
