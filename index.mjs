48b26fbc1a43e8f898dd424b68ebe958c3153c3d
Richard Neves <richard.albuquerqueneves@gmail.com>
2023-06-22 23:06:08 -0300

    Buscando objetos do s3

--- index.mjs
+++ index.mjs
@@ -1,11 +1,34 @@
 import { log } from './log.mjs';
+import { s3 } from '@aws-sdk/client-s3';
 
+const s3Client = new S3({ region: 'us-east-1' })
 export const handler = async(event) => {
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const key = record.s3.object.key;
+    const getObjectResult = await s3Client.getObject({
+        Bucket,
+        Key,
+    });
+    const mega_byte = 1024 * 1024;
 
-    log('event:' + JSON.stringify(event));
+    if (getObjectResult.ContentLength > 1 * mega_byte) {
+        log('Objeto Muito Grande');
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(event),
-    };
+        return 'Objeto Muito Grande';
+
+    }
+
+        log('Objeto de Tamanho OK');
+        return 'Objeto de Tamanhao OK';
+ 
+    // log('event:' + JSON.stringify(event));
+
+    // return {
+    //     statusCode: 200,
+    //     'body' => "<html><body>Dados da req: $req</body></html>",
+    //     'headers' => [
+    //         'Content-Type' => 'text/html', 
+    //   ],
+    // };
 };
